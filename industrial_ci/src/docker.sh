3faa4f3caa104c1146b2eb611b539a6b6516f2ef
Renato Samperio <renatosamperio@gmail.com>
2018-12-18 10:08:26 +0100

    Added docker group and Looking for usermod

--- industrial_ci/src/docker.sh
+++ industrial_ci/src/docker.sh
@@ -213,8 +213,8 @@ function ici_prepare_docker_image() {
   ici_time_start prepare_docker_image
   
   echo "  +++ DOCKER add user docker to system"
-  ls -la /bin/busybox
-  sudo groupadd docker
+  ls -la /bin/busybox/
+  sudo /bin/busybox/groupadd docker
   sudo usermod -aG docker $USER
 
   echo "  +++ DOCKER DOCKER_FILE: $DOCKER_FILE"
